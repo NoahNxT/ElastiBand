14f1105f474362b0cfe3b77fa5a55310579dbfa8
Noah Gillard <noah.gillard@student.kdg.be>
2020-03-23 21:25:13 +0100

    Change backend script mail to right mail

--- mail.php
+++ mail.php
@@ -1,17 +1,16 @@
 <?php
-    $to = "morshed08@gmail.com";
-    $from = $_REQUEST['name'];
-    $headers = "Content-type: text/html;From: $from";
+$to = "noah.gillard@student.kdg.be";
+$from = $_REQUEST['name'];
+$headers = "Content-type: text/html;From: $from";
 
-    $fields = array();
-    $fields["name"] = $_REQUEST['name'];
-    $fields["email"] = $_REQUEST['email'];
-    $fields["message"] = $_REQUEST['message'];
+$fields = array();
+$fields["name"] = $_REQUEST['name'];
+$fields["email"] = $_REQUEST['email'];
+$fields["message"] = $_REQUEST['message'];
 
-    $body = "Here is what was sent:\n\n";
-    $body .= 'Name : '.$fields['name']. '<br>';
-    $body .= 'Email : '.$fields['email']. '<br>';
-    $body .= 'Message : '.$fields['message']. '<br>';
+$body = "Here is what was sent:\n\n";
+$body .= 'Name : ' . $fields['name'] . '<br>';
+$body .= 'Email : ' . $fields['email'] . '<br>';
+$body .= 'Message : ' . $fields['message'] . '<br>';
 
-    $send = mail($to, $body, $headers);
-
+$send = mail($to, $body, $headers);
